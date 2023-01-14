b675c0a3411e0b824e49807e19a46be72321044c
dawkaka <yousiph77@gmail.com>
2023-01-14 20:07:20 +0000

    feat: layout

---
+++ src/components/board.tsx
@@ -0,0 +1,7 @@
+export default function Canvas() {
+    return (
+        <canvas className="ml-[300px] w-[calc(100%-300px)] h-[100%]">
+
+        </canvas>
+    )
+}
---
+++ src/components/side.tsx
@@ -0,0 +1,8 @@
+
+export default function Side() {
+    return (
+        <aside className="w-[300px] bg-neutral-50 fixed top-0 left-0 bottom-0 border-r border-neutral-200 overflow-auto">
+
+        </aside>
+    )
+}

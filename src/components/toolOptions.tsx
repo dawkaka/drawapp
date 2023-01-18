7949df493d72de142d2287f3330267f7935b982b
dawkaka <yousiph77@gmail.com>
2023-01-18 11:50:07 +0000

    feat: opacity full width

--- src/components/toolOptions.tsx
+++ src/components/toolOptions.tsx
@@ -79,9 +79,9 @@ export function FontFamily() {
 export function Opacity() {
     const [opacity, setOpacity] = useState(1)
     return (
-        <div>
+        <div className="w-full">
             <h6 className="text-sm text-[var(--accents-5)] mb-1">Opacity</h6>
-            <input type="range" className="accent-[darkorange] cursor-resize" onChange={(e) => setOpacity(e.target.valueAsNumber)} value={opacity} min={0} max={1} step={0.1} />
+            <input type="range" className="accent-[darkorange] cursor-resize w-full" onChange={(e) => setOpacity(e.target.valueAsNumber)} value={opacity} min={0} max={1} step={0.1} />
         </div>
     )
 }

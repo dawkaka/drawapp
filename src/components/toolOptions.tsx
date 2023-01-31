a0bd4aa22befabec220c7bc96a3214be9d8f6fa5
dawkaka <yousiph77@gmail.com>
2023-01-31 22:39:34 +0000

    flip v

--- src/components/toolOptions.tsx
+++ src/components/toolOptions.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai"
-import { useEffect, useMemo, useState } from "react"
+import { useState } from "react"
 import { AppState, AppDrawings, SelectionAtom } from "../jotai"
 import { getSelectedItem, moveItemPosition, updateSingleItem } from "../lib/utils"
 import type { LayerMoves, Stroke, StrokeWidth } from "../types"
@@ -54,7 +54,7 @@ export function ImageOptions() {
 
             <div className="flex items-center gap-2" onClick={() => flip("y")}>
                 <OptionContainer selected={""} value="25" onClick={() => { }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.97 15.25h-2.72c-5.3 0-9.5-2.15-9.5-4.5s4.2-4.5 9.5-4.5c3.03 0 5.82.7 7.62 1.86a.75.75 0 1 0 .81-1.26c-2.06-1.33-5.13-2.1-8.43-2.1-6.02 0-11 2.55-11 6s4.98 6 11 6h2.8l-2.3 2.3a.75.75 0 1 0 1.07 1.05l2.83-2.82c.68-.69.68-1.8 0-2.48l-2.83-2.83a.75.75 0 0 0-1.06 1.06l2.21 2.22z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.25 8.35v2.4c0 5.3-2.15 9.5-4.5 9.5s-4.5-4.2-4.5-9.5c0-3.03.7-5.82 1.86-7.62a.75.75 0 1 0-1.26-.81c-1.33 2.06-2.1 5.13-2.1 8.43 0 6.02 2.55 11 6 11s6-4.98 6-11V8.27l2.3 2.3A.75.75 0 1 0 20.1 9.5l-2.82-2.83a1.75 1.75 0 0 0-2.48 0L11.97 9.5a.75.75 0 1 0 1.06 1.06l2.22-2.22z"></path></svg>
                 </OptionContainer>
                 <span className="text-neutral-500">Flip Vertically</span>
             </div>

8c58681bdc69708fc6ac8fc79c11890a734c5d0a
dawkaka <yousiph77@gmail.com>
2023-04-18 21:02:45 +0000

    feat: remove vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
